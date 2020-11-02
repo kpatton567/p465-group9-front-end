bfa5f571f4a80a4b6663417e74614fa83604e9b6
Ian Newhart <inewhart@iu.edu>
2020-11-01 21:42:52 -0500

    Transaction progress

--- reactjs/src/modules/views/TransactionPage.js
+++ reactjs/src/modules/views/TransactionPage.js
@@ -1,219 +1,223 @@
-import withRoot from './../withRoot';
-// --- Post bootstrap -----
-import React, {useState} from 'react';
-import AppFooter from './../views/AppFooter';
-import AppAppBar from './../views/AppAppBar';
-import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
+import React from "react";
+import clsx from 'clsx';
+import { Row, Col } from 'reactstrap';
+import axios from 'axios';
+import {ACCESS_TOKEN_NAME, apiVariables} from '../../APIConstants';
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import { useAuth0 } from '@auth0/auth0-react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Title from './Title';
-import clsx from 'clsx';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Container from '@material-ui/core/Container';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import { mainListItems } from './listItems';
 
-const drawerWidth = 250;
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    backgroundColor: '#363636',
-    // whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(1),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
-}));
+// core components
+import Typography from '../components/Typography';
+import GridContainer from "../components/GridContainer.js";
+import GridItem from "../components/GridItem.js";
+import Parallax from "../components/Parallax.js";
+import styles from "./BookingHistPageStyles.js";
+import AppFooter from '../../modules/views/AppFooter';
+import AppAppBar from "./AppAppBar";
 
 
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+const useStyles = makeStyles(styles);
+
+// Generate Order Data
+function createData(paymentId, date, movieName, theaterName, snacks, total) 
+{
+    return         {paymentId, date, movieName, theaterName, snacks, total};
 }
 
-const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
-];
 
 function preventDefault(event) {
-  event.preventDefault();
+    event.preventDefault();
 }
 
-axios.get('http://localhost:8080/api/manage/transaction_history', payload)
-      .then(function (response) {
-        
+
+
+// Sample data for testing
+const snackz1 = ['Pizza, ', 'Coca-Cola, ', 'Chocolate'];
+const orderz = [
+    createData('1', '16 Sept, 2019', 'Avengers: Endgame', 'AMC', snackz1, 30.44),
+];
+
+
+var token = localStorage.getItem(ACCESS_TOKEN_NAME);
+
+
+export default function BookingHistoryPage(props) 
+{
+  // Bring in data from backend
+  const [orders, setOrders] = React.useState([]);
+  const fetchData = React.useCallback(() => 
+  {
+      axios.get(apiVariables.apiUrl + '/api/customer/payment_history', {
+        headers: {
+        "Authorization": 'Bearer ' + token
+        }
       })
-      .catch(function (error) {
-        console.log(error);
-      });
-export default function TransactionPage() {
-  const [state, setState] = useState({
-    movieTitle: "",
-    movieDesc: "",
-    movieURL: "",
-    movieGenre: ""
-  })
-  const handleChange = (e) => {
-    const { id, value } = e.target
-      setState(prevState => ({
-        ...prevState,
-        [id]: value
-      }))
-    }
-    const handleSubmit = () => {
-      setSaved(true);
-    };
-    const [sent, setSaved] = React.useState(false);
-    const [open, setOpen] = React.useState(true);
-    const handleDrawerOpen = () => {
-      setOpen(true);
-    };
-    const handleDrawerClose = () => {
-      setOpen(false);
-    };
-    const handleSubmitClick = (e) => {
-      e.preventDefault();}
+      .then((response) =>
+        {
+          var i = 0;
+          var j = 0;
+
+          for(i = 0; i < response.data.length; i++)
+          {
+            // Create an array with all of the snacks from each order (to be passed to createData)
+            const snackz = [];
+            for (j = 0; j < response.data[i].snacks.length; j++)
+            {
+              snackz[j] = response.data[i].snacks[j] + ' '; 
+            }
+
+            orders[i] = createData(response.data[i].paymentId, response.data[i].date, response.data[i].movieName,
+                                   response.data[i].theaterName, snackz, response.data[i].total);
+          }
+        },
+        (error) => {
+          var status = error.response.status;
+          console.log(status);
+        }
+      );
+  }, []);
+
+  React.useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+
   const classes = useStyles();
-  return (
-    <div className={classes.root}
-    style={{background: '#808080'}}>
-      <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <AppAppBar />
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>{mainListItems}</List>
-        <Divider />
-      </Drawer>
-      <Title>Recent Orders</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <div className={classes.seeMore}>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          See more orders
-                </Link>
-      </div>
-      <main className={classes.content}
-      style={{background: '#808080'}}>
-      </main>
-      </div>
+  const { ...rest } = props;
+  const { user, isAuthenticated, loginWithRedirect, isLoading, getIdTokenClaims } = useAuth0();
+ 
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
   );
+
+  if (isAuthenticated) {
+    user.app_metadata = user.app_metadata || {};
+    console.log(user)
+  }
+
+  const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+  let ETTprops = {}
+  if (user) {
+    ETTprops = {
+      email: user.email
+    }
+  }
+
+  if (!isAuthenticated && isLoading) {
+    return (<div>
+      Loading
+    </div>)
+  }
+
+  if (!isAuthenticated && !isLoading) {
+    return (<div>
+      Loading
+    {loginWithRedirect()}
+    </div>)
+  }
+
+
+
+  // Generate page
+  if (isAuthenticated && !isLoading) 
+  { 
+    return (
+        <div>
+          <AppAppBar position="relative" />
+
+          {/* background image */}
+          <Parallax small filter image={require("../assets/popcorn.jpg")} />
+
+          {/* Raised background page, profile image, customer tag */}
+          <div className={classNames(classes.main, classes.mainRaised)}>
+            <div>
+              <div className={classes.container}>
+                <GridContainer justify="center">
+                  <GridItem xs={12} sm={12} md={6}>
+                    <div className={classes.profile}>
+                      <div>
+                        <img src={user.picture} alt="..." className={imageClasses} />
+                      </div>
+                      <div className={classes.name}>
+                        <h3 className={classes.title}>{user.nickname}</h3>
+                        <h6>CUSTOMER</h6>
+                      </div>
+                    </div>
+                  </GridItem>
+                </GridContainer>
+              </div>
+            </div>
+
+
+            {/* Text below profile image */}
+            <div>
+              <Typography className={classes.title2} align="center" variant="h3" marked="center">
+                {'TRANSACTION HISTORY'}
+              </Typography>
+            </div>
+            <div>
+              <Typography className={classes.subtext} align="center" variant="h5">
+                {''}
+              </Typography>
+            </div>
+
+
+            {/* Order History Table */}
+            <Grid item xs={12}>
+                <div className={classes.table}>
+                <Paper className={classes.paper}>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Order Number</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Movie Name</TableCell>
+                                <TableCell>Theatre Name</TableCell>
+                                <TableCell>Snacks</TableCell>
+                                <TableCell align="right">Total Bill</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {orders.map((order) => (
+                                <TableRow key={order.paymentId}>
+                                    <TableCell>{order.paymentId}</TableCell>
+                                    <TableCell>{order.date}</TableCell>
+                                    <TableCell>{order.movieName}</TableCell>
+                                    <TableCell>{order.theaterName}</TableCell>
+                                    <TableCell>{order.snacks}</TableCell>
+                                    <TableCell align="right">{order.total}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+                </div>
+            </Grid>
+            <div>
+              <Typography className={classes.subtext2} align="center" variant="h5">
+                {''}
+              </Typography>
+            </div>
+ 
+          </div>
+          {/* End main raised page */}
+
+
+          {/* Footer */}
+          <AppFooter />
+        </div>
+      );
+  }
 }
